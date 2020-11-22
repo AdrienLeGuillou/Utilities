d7e5369429acc873c237eef1efd602b0b06ed015
Adrien Le Guillou <adrien.leguillou@gmail.com>
2020-11-22 17:10:08 +0100

    add startup

--- renv_perma_lib.sh
+++ renv_perma_lib.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# 
+#
 # Author: Adrien Le Guillou
 #
 # Link some R libraries from the main user library to a secondary one.
@@ -8,16 +8,17 @@
 #
 # RENV_CONFIG_EXTERNAL_LIBRARIES=~/R/x86_64-pc-linux-gnu-library/renv_utils_lib
 
-UTILS_PACKAGES="nvimcom 
-colorout 
-lintr"
+UTILS_PACKAGES="nvimcom
+colorout
+lintr
+startup"
 
 CURRENT_R_LIB=$(Rscript -e ".libPaths()" | grep $HOME)
 CURRENT_R_LIB=${CURRENT_R_LIB#* \"} # remove [1] "
 CURRENT_R_LIB=${CURRENT_R_LIB%\"} # remove closing "
 
 # renv_utils_lib is in the same folder as `4.0`
-RENV_R_LIB="${CURRENT_R_LIB%/*}/renv_utils_lib" 
+RENV_R_LIB="${CURRENT_R_LIB%/*}/renv_utils_lib"
 
 rm -rf "$RENV_R_LIB"
 mkdir "$RENV_R_LIB"
